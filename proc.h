cc161d531e826612e3feed20df3dbf76b9c9471f
David Phan <dphan015@sledge.cs.ucr.edu>
2016-04-21 21:56:10 -0700

    proc.h : added status variable

--- proc.h
+++ proc.h
@@ -66,6 +66,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+
+  int status;                  // maintains the exit status of a system call
 };
 
 // Process memory is laid out contiguously, low addresses first:
