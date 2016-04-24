4e2b1d2f099e3840b583065d3b1ad1d328cf7383
John Phang <jphan016@sledge.cs.ucr.edu>
2016-04-24 01:23:25 -0700

    Lab 1 Part B Getting error while make qemu-nox

--- proc.c
+++ proc.c
@@ -206,7 +206,7 @@ exit(int status)
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
 int
-wait(void)
+wait(int *status)
 {
   struct proc *p;
   int havekids, pid;
@@ -220,7 +220,7 @@ wait(void)
         continue;
       havekids = 1;
       if(p->state == ZOMBIE){
-        // Found one.
+        //Found one.
         pid = p->pid;
         kfree(p->kstack);
         p->kstack = 0;
@@ -230,6 +230,10 @@ wait(void)
         p->parent = 0;
         p->name[0] = 0;
         p->killed = 0;
+
+        if(status){
+            status = &p->status;
+        }
         release(&ptable.lock);
         return pid;
       }
