695a666c0656f47afbe51be1b36589e17595e4c3
David Phan <dphan015@sledge.cs.ucr.edu>
2016-04-23 23:34:17 -0700

    modified wait()

--- defs.h
+++ defs.h
@@ -113,7 +113,7 @@ void            scheduler(void) __attribute__((noreturn));
 void            sched(void);
 void            sleep(void*, struct spinlock*);
 void            userinit(void);
-int             wait(void);
+int             wait(int *status);      // lab1 - part b
 void            wakeup(void*);
 void            yield(void);
 
